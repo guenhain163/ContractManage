22ab11f43bec2ef943a2195c733b0513b0eb7343
Hain <hanh1632001@gmail.com>
2024-05-09 20:09:41 +0700

    second

--- tests/ContractManager.spec.ts
+++ tests/ContractManager.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
+import { address, toNano } from '@ton/core';
 import { ContractManager } from '../wrappers/ContractManager';
 import '@ton/test-utils';
 
@@ -37,8 +37,44 @@ describe('ContractManager', () => {
     it('getBalance', async () => {
         // the check is done inside beforeEach
         // blockchain and contractManager are ready to use
-        const sender = await blockchain.treasury('sender');
         const blancer = await contractManager.getBalance();
+
+        expect(blancer).toEqual(0n);
+    });
+
+    it('add participant', async () => {
+        // the check is done inside beforeEach
+        // blockchain and contractManager are ready to use
+        const sender = await blockchain.treasury('sender');
+        const participantAddress = address("EQBGhqLAZseEqRXz4ByFPTGV7SVMlI4hrbs-Sps_Xzx01x8G");
+        const participantName = 'Hain';
+        // const participantBalance = 50n; // Assuming initial balance
+
+        const deployResult = await contractManager.send(
+            sender.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'ParticipantCreate',
+                walletAddress: participantAddress,
+                name: participantName,
+                // balance: participantBalance,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: sender.address,
+            to: contractManager.address,
+            deploy: false,
+            success: true,
+        });
+
+        // Verify participant addition
+        // const participant = await contractManager.getParticipant(participantAddress);
+        // expect(participant).not.toBeNull(); // Check participant exists
+        // expect(participant?.name).toEqual(participantName);
+        // expect(participant.balance).toEqual(participantBalance); // Check initial balance (if applicable)
     });
 
 });
